5b655c1f6f3eccd4d74ee2d316332ddabf9b7f62
ejjigiripraveenkumar99 <ejjigiripraveenkumar99@gmail.com>
2026-06-09 14:21:10 +0000

    Completed simple DFF cocotb verification

---
+++ sources/dff.sv
@@ -0,0 +1,18 @@
+
+`timescale 1ns/1ps
+
+module dff (
+    input  logic clk,
+    input  logic rst_n,
+    input  logic d,
+    output logic q
+);
+
+always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+        q <= 1'b0;
+    else
+        q <= d;
+end
+
+endmodule
